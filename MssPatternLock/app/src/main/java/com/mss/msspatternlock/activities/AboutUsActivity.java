699c17ae856df1bf382796502569592d29279da3
githubmss <github@mastersoftwaresolutions.com>
2016-12-09 12:27:06 +0530

    Add contact,about screen and keystore

---
+++ MssPatternLock/app/src/main/java/com/mss/msspatternlock/activities/AboutUsActivity.java
@@ -0,0 +1,82 @@
+package com.mss.msspatternlock.activities;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.mss.msspatternlock.R;
+
+/**
+ * Created by hp on 30/11/16.
+ */
+public class AboutUsActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    private ProgressDialog pd;
+    private WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        toolbar = (Toolbar) findViewById(R.id.custum_bar);
+        TextView toolbarTitle = (TextView) findViewById(R.id.txt_title);
+        toolbarTitle.setText("About Us");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        webView = (WebView) findViewById(R.id.web_about);
+        pd = new ProgressDialog(AboutUsActivity.this);
+        pd.setMessage("loading...");
+        pd.show();
+        webView.setWebViewClient(new myWebClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("http://www.mastersoftwaresolutions.com/");
+        webView.getSettings().setBuiltInZoomControls(true);
+    }
+
+    public class myWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            pd.show();
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            pd.dismiss();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(AboutUsActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+        }
+        return (super.onOptionsItemSelected(menuItem));
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+}
--- MssPatternLock/app/src/main/java/com/mss/msspatternlock/utils/Constants.java
+++ MssPatternLock/app/src/main/java/com/mss/msspatternlock/utils/Constants.java
@@ -5,8 +5,7 @@ package com.mss.msspatternlock.utils;
  */
 
 public class Constants {
-
     public static final String PATTERN_SHA_VALUE = "patternSha";
-
-
+    public static String CURRENT_LATITUDE = "current_lat";
+    public static String CURRENT_LONGITUDE = "current_lng";
 }
